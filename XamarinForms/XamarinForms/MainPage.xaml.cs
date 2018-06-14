abbdbdf8459469cb29c1882938bc0bad960ae156
Tomáš Wagner <t.e.n.k@seznam.cz>
2018-06-15 00:07:01 +0200

    Label, Editor, Button and ListView added, with data binding

--- XamarinForms/XamarinForms/MainPage.xaml.cs
+++ XamarinForms/XamarinForms/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,31 @@ namespace XamarinForms
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        public ObservableCollection<string> Data { get; set; }
+
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            Data = new ObservableCollection<string>
+            {
+                "Něco",
+                "Nic",
+                "Vše",
+            };
+            //BtnAdd.
+            //ListViewData.ItemsSource = Data;
+        }
+
+        void BtnAdd_Clicked_Handler(object sender, EventArgs eventArgs)
+        {
+            Data.Add(EditorNewItem.Text);
+            EditorNewItem.Text = string.Empty;
+        }
+
+        private void ListViewData_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Data.RemoveAt(Data.IndexOf(e.SelectedItem.ToString()));
+        }
+    }
 }
