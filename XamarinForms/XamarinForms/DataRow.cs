7b3ba79c5929336460fa0631df92acfc11646d3c
Tomáš Wagner <wagner@havit.cz>
2018-06-17 00:58:36 +0200

    Binding, Toast

---
+++ XamarinForms/XamarinForms/DataRow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinForms
+{
+	//[ImplementPropertyChanged]
+	public class DataRow
+    {
+		public string Val { get; set; }
+	}
+}
--- XamarinForms/XamarinForms/MainPage.xaml.cs
+++ XamarinForms/XamarinForms/MainPage.xaml.cs
@@ -5,36 +5,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace XamarinForms
 {
+	//[ImplementPropertyChanged]
 	public partial class MainPage : ContentPage
 	{
-        public ObservableCollection<string> Data { get; set; }
+        public ObservableCollection<DataRow> Data { get; set; }
+		string tag = $"XamarinForms-{nameof(MainPage)}";
 
         public MainPage()
 		{
 			InitializeComponent();
 
-            Data = new ObservableCollection<string>
-            {
-                "Něco",
-                "Nic",
-                "Vše",
-            };
-            //BtnAdd.
-            //ListViewData.ItemsSource = Data;
-        }
+            //Data = new ObservableCollection<string>
+            //{
+            //    "Něco",
+            //    "Nic",
+            //    "Vše",
+            //};
+			Data = new ObservableCollection<DataRow>
+			{
+				new DataRow { Val = "Něco" },
+				new DataRow { Val = "Nic" },
+				new DataRow { Val = "Vše" }
+			};
+
+			All = new List<DataRow>();
+			foreach (var item in Data)
+			{
+				All.Add(item);
+			}
+			
+			ListViewData.ItemsSource = Data;
+		}
 
         void BtnAdd_Clicked_Handler(object sender, EventArgs eventArgs)
         {
-            Data.Add(EditorNewItem.Text);
-            EditorNewItem.Text = string.Empty;
-        }
+            Data.Add(new DataRow { Val = EditorNewItem.Text });
+            All.Add(new DataRow { Val = EditorNewItem.Text });
+
+			Log.Warning(tag, $"{nameof(BtnAdd_Clicked_Handler)}: {EditorNewItem.Text} added");
+			Log.Warning(tag, $"{nameof(BtnAdd_Clicked_Handler)}: Data.Count = {Data.Count()}");
+			Console.WriteLine($"{nameof(BtnAdd_Clicked_Handler)}: {EditorNewItem.Text} added");
+			Console.WriteLine($"{nameof(BtnAdd_Clicked_Handler)}: Data.Count = {Data.Count()}");
+
+			DependencyService.Get<IToast>().Show($"'{EditorNewItem.Text}' bylo přidáno.");
+
+			EditorNewItem.Text = string.Empty;
+		}
 
         private void ListViewData_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            Data.RemoveAt(Data.IndexOf(e.SelectedItem.ToString()));
+            //Data.RemoveAt(Data.IndexOf(e.SelectedItem.ToString()));
+            Data.RemoveAt(Data.IndexOf((e.SelectedItem as DataRow)));
+            All.RemoveAt(All.IndexOf((e.SelectedItem as DataRow)));
         }
-    }
+
+		public static IList<DataRow> All { private set; get; }
+
+		private void ToastButton_Clicked(object sender, EventArgs e)
+		{
+			// samotná implementace zobrazení toastu je platform-specific
+			DependencyService.Get<IToast>().Show("Toast Message");
+		}
+	}
 }
